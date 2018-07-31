6184bddd2d1ff41aabf17415499516a6f1f26a4e
Meck Tinum <meck.tinum@hotmail.com>
2018-07-31 14:43:07 +0100

    Created the navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import News from './components/news/News';
+import Navbar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
      <Router>
        <div>
+         <Navbar />
          <Route exact path="/" component={Home} />
          <Route path="/about" component={About} />
          <Route path="/news" component={News} />
